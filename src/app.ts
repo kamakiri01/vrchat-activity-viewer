960834ed07ff6328a4d5daac1b315b02610c66bd
kamakiri01 <kamakiri01@users.noreply.github.com>
2021-01-11 05:08:00 +0900

    update filter

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import * as path from "path";
 import { initDatabase, loadDatabase, existDatabaseFile, findVRChatLogFilesFromDirPath, loadVRChatLogFile, parseVRChatLog, mergeActivityLog, writeDatabase } from "./util";
-import { ActivityLog, MoveActivityLog, EnterActivityLog } from "./type";
+import { ActivityLog, MoveActivityLog, EnterActivityLog, Database } from "./type";
 
 const DEFAULT_VRCAT_PATH = "/AppData/LocalLow/VRChat/VRChat";
 
@@ -19,26 +19,45 @@ export function app(param: appParameterObject) {
     }
 
     const db = loadDatabase(dbPath);
-    const vrchaLogPath = db.vrchatHomePath;
+    let vrchaLogPath = db.vrchatHomePath;
+
+    if (param.import) {
+        vrchaLogPath = path.join(process.cwd(), param.import);
+        console.log("search log files from " + vrchaLogPath);
+    }
+
+    const newDbLog = updateDb(db, dbPath, vrchaLogPath);
+
+    if (param.import) return;
+    showLog(param, newDbLog);
+}
+
+function updateDb(db: Database, dbPath: string, vrchaLogPath: string) {
     console.log("searching vrchat log files...")
     const filePaths = findVRChatLogFilesFromDirPath(vrchaLogPath);
-    console.log("find vrchat log file(s): " + filePaths.map(filePath => path.basename(filePath)).join(", "));
+    console.log("find "  + filePaths.length + " log file(s): " + filePaths.map(filePath => path.basename(filePath)).join(", "));
     const logs = filePaths.map((filePath) => {
         return parseVRChatLog(loadVRChatLogFile(path.join(vrchaLogPath, filePath)));
     });
     const newLog: ActivityLog[] = Array.prototype.concat.apply([], logs);
     const currentLogLength = db.log.length;
     const newDbLog = mergeActivityLog(db.log, newLog);
-    console.log("update " + (newDbLog.length - currentLogLength) + " logs");
+    console.log("update new " +
+        (
+            (Number.isNaN(newDbLog.length) ? 0 : newDbLog.length) -
+            (Number.isNaN(currentLogLength) ? 0 : currentLogLength)
+        ) + " logs");
     db.log = newDbLog;
     writeDatabase(dbPath, JSON.stringify(db, null, 2));
     console.log("update DB done");
+    return newDbLog;
+}
 
-    // show log
+function showLog(param: appParameterObject, activityLog: ActivityLog[]): void {
     console.log("--- Activity Log ---")
     const currentTime = Date.now();
     const daymillisecond = 24 * 60 * 60 * 1000;
-    const showLog = newDbLog.filter(e => currentTime - e.date < daymillisecond);
+    const showLog = activityLog.filter(e => currentTime - e.date < daymillisecond);
     // const showLog = newDbLog;
     showLog.forEach(e => {
         const date = new Date(e.date);
@@ -52,6 +71,7 @@ export function app(param: appParameterObject) {
                 message += e.activityType + " " + (<EnterActivityLog>e).worldname;
                 break;
         }
+        if (param.filter && message.indexOf(param.filter) === -1) return;
         console.log(message);
         // console.log(date.toLocaleDateString() + " " + date.toLocaleTimeString() + " " + e.activityType);
     })
