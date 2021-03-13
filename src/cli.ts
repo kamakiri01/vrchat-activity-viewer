dbf8e0a3dfd10429c5cc7001a0f9f7ac8e19941b
kamakiri01 <kamakiri01@users.noreply.github.com>
2021-03-13 22:52:02 +0900

    add case-filter option

--- src/cli.ts
+++ src/cli.ts
@@ -6,7 +6,8 @@ program
 
 program
     .description("VRChat log viewer")
-    .option("-f, --filter <word...>", "filter logs with words")
+    .option("-f, --filter <word...>", "filter logs with ignore case words")
+    .option("-cf, --case-filter <word...>", "filter logs with no ignore case words")
     .option("-i, --import <dir>", "log directory to import additional")
     .option("-V, --verbose", "display full log details")
     .option("-r, --range <hours>", "specify the range to display", "24")
@@ -16,6 +17,7 @@ export async function run(argv: any): Promise<void> {
     app({
         import: program["import"],
         filter: program["filter"],
+        caseFilter: program["caseFilter"],
         verbose: program["verbose"],
         range: program["range"]
     });
