3b6b43a789c29f998565a42be33e0f260451bf39
kamakiri01 <kamakiri01@users.noreply.github.com>
2022-02-12 00:08:38 +0900

    type strictly

--- src/type/ActivityLogType/systemType.ts
+++ src/type/ActivityLogType/systemType.ts
@@ -1,23 +1,27 @@
 import { RegionType, WorldAccessScope } from "../common";
-import { ActivityLog } from "./common";
+import { ActivityLog, ActivityType } from "./common";
 
 // world in
 export interface EnterActivityLog extends ActivityLog {
+    activityType: typeof ActivityType.Enter;
     worldData: WorldLogData;
 }
 
 // login
 export interface AuthenticationActivityLog extends ActivityLog {
+    activityType: typeof ActivityType.Authentication;
     userName: string;
 }
 
 // shotdown
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ShutdownActivityLog extends ActivityLog {
+    activityType: typeof ActivityType.Shutdown
     // nothing
 }
 
 export interface CheckBuildActivityLog extends ActivityLog {
+    activityType: typeof ActivityType.CheckBuild
     buildName: string;
 }
 
